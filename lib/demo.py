e7a01d27f36a3a1bdb37e200cc0adfe07a8b9fe4
Ihor Stepanov <igor.stepanov2000@gmail.com>
2022-12-01 21:10:32 +0200

    optimize code

--- lib/demo.py
+++ lib/demo.py
@@ -1,24 +1,21 @@
-from lib.structs import Value, Tensor
-from lib.nn import Linear, ReLU, MSE
-from lib.optim import SGD, Adam
+from lib.structs import Tensor
+from lib.nn import Linear, MSE
+from lib.optim import Adam
 from tqdm import tqdm
 
+data = Tensor.rand((10, 100), dtype = float)
 
-def test_linear():
-    data = Tensor.rand((10, 100), dtype = float)
+targets = Tensor.rand((10, 1), dtype = float)
 
-    targets = Tensor.rand((10, 1), dtype = float)
+model = Linear(100, 1)
 
-    model = Linear(100, 1)
+criterion = MSE()
 
-    criterion = MSE()
+optimizer = Adam(model.parameters(), lr = 0.01)
 
-    optimizer = SGD(model.parameters(), lr = 0.01)
-
-    for _ in tqdm(range(1000)):
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, targets)
-        loss.backward()
-        optimizer.step()
-        print(loss)
+for epoch in tqdm(range(10)):
+    optimizer.zero_grad()
+    output = model(data)
+    loss = criterion(output, targets)
+    loss.backward()
+    optimizer.step()
--- test/test_demo.py
+++ test/test_demo.py
@@ -1,22 +1,24 @@
-from lib.structs import Value, Tensor
-from lib.nn import Linear, ReLU, MSE
-from lib.optim import SGD, Adam
-from tqdm import tqdm
+from lib.structs import Tensor
+from lib.nn import Linear, MSE
+from lib.optim import SGD
 
-data = Tensor.rand((10, 100), dtype = float)
 
-targets = Tensor.rand((10, 1), dtype = float)
+def test_linear():
+    data = Tensor.rand((10, 100), dtype = float)
 
-model = Linear(100, 1)
+    targets = Tensor.rand((10, 1), dtype = float)
 
-criterion = MSE()
+    model = Linear(100, 1)
 
-optimizer = Adam(model.parameters(), lr = 0.01)
+    criterion = MSE()
 
-for epoch in tqdm(range(1000)):
-    optimizer.zero_grad()
-    output = model(data)
-    loss = criterion(output, targets)
-    loss.backward()
-    optimizer.step()
-    print(loss)
+    optimizer = SGD(model.parameters(), lr = 0.01)
+
+    for _ in range(10):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, targets)
+        loss.backward()
+        optimizer.step()
+        print(loss)
+
